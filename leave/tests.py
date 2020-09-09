ba85c0802801cfb7576f0cecad5371701e380218
Michael Frohlich <mike.frohl@gmail.com>
2020-09-09 19:56:38 +0200

    add test

--- leave/tests.py
+++ leave/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+from leave.models import Employee
+
+
+class EmployeeTest(TestCase):
+    def setUp(self):
+        self.username = "testuser"
+        self.employee_number = "D01234"
+        self.phone_number = 27844444849
+        self.first_name = "Test"
+        self.last_name = "User"
+        self.password = "z"
+
+        self.test_user = Employee.objects.create(
+            emp_number=self.username,
+            phone_number=self.phone_number,
+            first_name=self.first_name,
+            last_name=self.last_name
+        )
+
+    def test_create_employee(self):
+        assert isinstance(self.test_user, Employee)
